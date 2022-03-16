d574494462df9829de3de5503f7f9d3a035bd0a1
Helton Bustos <heltonsmith@hotmail.com>
2022-03-16 18:31:51 -0300

    Agregué NavigationDrawer y Fragmentos

---
+++ app/src/main/java/com/heltonbustos/ejemplomvp01/view/FragEliminarRegistroEquipos.java
@@ -0,0 +1,21 @@
+package com.heltonbustos.ejemplomvp01.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.heltonbustos.ejemplomvp01.R;
+
+public class FragEliminarRegistroEquipos extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_eliminar_registro_equipos, container, false);
+    }
+}
--- app/src/main/java/com/heltonbustos/ejemplomvp01/view/OtraActividad.java
+++ app/src/main/java/com/heltonbustos/ejemplomvp01/view/OtraActividad.java
@@ -1,25 +1,105 @@
 package com.heltonbustos.ejemplomvp01.view;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.heltonbustos.ejemplomvp01.R;
 
-public class OtraActividad extends AppCompatActivity {
+import org.w3c.dom.Text;
 
-    TextView txtNombre2;
+public class OtraActividad extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout myDrawer;
+    NavigationView myNav;
+    Toolbar myToolbar;
+
+    ActionBarDrawerToggle toogle; //para implementar el icono de hamburguesa
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otra_actividad);
 
-        txtNombre2 = findViewById(R.id.txtNombre2);
+        myDrawer = findViewById(R.id.myDrawerLayout);
+        myNav = findViewById(R.id.myNavigationView);
+        myToolbar = findViewById(R.id.myToolbar);
 
         String nombre = getIntent().getStringExtra("x");
 
-        txtNombre2.setText("Bienvenido: " + nombre);
+        //mostrar actionbar
+        setSupportActionBar(myToolbar);
+
+        //eventos click en items de navigationDrawer
+        myNav.setNavigationItemSelectedListener(this);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.myFrame, new FragRegistroEquipo())
+                .commit();
+        setTitle("Registrar equipo");
+
+        //para activar icono hamburguesa
+        //toogle = new ActionBarDrawerToggle(this, myDrawer, myToolbar, R.string.drawer_open, R.string.drawer_close);
+        toogle = setDrawerToogle();
+        myDrawer.addDrawerListener(toogle); //para oir al icono de hamburguesa
+
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        toogle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        toogle.syncState();
+    }
+
+    private ActionBarDrawerToggle setDrawerToogle() {
+        return new ActionBarDrawerToggle(this, myDrawer, myToolbar, R.string.drawer_open, R.string.drawer_close);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toogle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        //para mostrar los fragmentos
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        switch (item.getItemId()) {
+            case R.id.nav_registro:
+                ft.replace(R.id.myFrame, new FragRegistroEquipo()).commit();
+                break;
+            case R.id.nav_ver:
+                ft.replace(R.id.myFrame, new FragVerEquipos()).commit();
+                break;
+            case R.id.nav_eliminar:
+                ft.replace(R.id.myFrame, new FragEliminarRegistroEquipos()).commit();
+                break;
+        }
+        setTitle(item.getTitle()); //para mostrar el título
+        myDrawer.closeDrawers(); //para cerrar drawer
+        return true;
     }
 }
